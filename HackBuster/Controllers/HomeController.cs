ec7f845376c66e17d398ab153684d639c531e702
OppaPlzHajima <51772642+OppaPlzHajima@users.noreply.github.com>
2019-10-16 02:47:09 -0400

    Commiting second time

---
+++ HackBuster/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HackBuster.Models;
+
+namespace HackBuster.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult Result(User user)
+        {
+            ViewBag.Name = user.FirstName;
+            ViewBag.FirstName = user.FirstName;
+            return View();
+        }
+
+
+        public IActionResult RegisterItem()
+        {
+            return View();
+        }
+        public IActionResult ItemResultView()
+        {
+            return View();
+        }
+        //[HttpPost]
+        ////public IActionResult ItemResultView(UserItem userItem)
+        ////{
+        ////    if (ModelState.IsValid)
+        ////    {
+
+        ////        //ViewBag.Name = userItem.Name;
+        ////        //ViewBag.Price = userItem.Price;
+        ////        //ViewBag.Description = userItem.Description;
+        ////        //ViewBag.Quantity = userItem.Quantity;
+        ////        return View(userItem);
+
+        ////    }
+        ////    else
+        ////    {
+        ////        return View(userItem);
+
+        ////    }
+
+
+
+    
+    }
+}
---
+++ HackBuster/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackBuster.Models
+{
+    public class User
+    {
+        [Key]
+
+        [Required(ErrorMessage = "Please enter a creative username")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password")]
+        [StringLength(100, ErrorMessage = "Password must be between 2 to 100 characters", MinimumLength = 2)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please enter your First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Your Last Name is Required")]
+        public string LastName { get; set; }
+        
+        [Required(ErrorMessage = "Social Security # is required")]
+        public string SSN { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "What's Your Favorite movie?")]
+        public string FavMovie { get; set; }
+
+
+    }
+}
